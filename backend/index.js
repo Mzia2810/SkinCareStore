520d62c3a752f9ae319e9d1061f4f5ea2aec2f0e
Ubuntu <azureuser@Skincare-linux.flbmz1hfco5ubhxdn3y3lprpub.rx.internal.cloudapp.net>
2026-03-08 01:21:34 +0000

    modified

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ const PORT = process.env.PORT || 5000;
 connectDB()
   .then(() => {
       console.log("MongoDB connected");
-      app.listen(PORT, () => {
+      app.listen(PORT,"0.0.0.0", () => {
           console.log(`Server running on port ${PORT}`);
       });
   })
