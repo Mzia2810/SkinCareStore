95403f7ac6b156513dd2e1fc2c3aef8a08f04632
Muhammad Zia <muhammadzia@Muhammads-MacBook-Pro.local>
2026-03-08 05:28:43 +0400

    port change

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.get('/', (req, res) => {
 });
 
 // Connect to MongoDB and start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 connectDB()
   .then(() => {
