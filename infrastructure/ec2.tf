6853604db78fc49204e5dc81662dededcd331999
Muhammad Zia <muhammadzia@Muhammads-MacBook-Pro.local>
2026-04-26 14:27:27 +0400

    terraform

--- infrastructure/ec2.tf
+++ infrastructure/ec2.tf
@@ -1,10 +1,8 @@
 resource "aws_instance" "web" {
-  ami                    = "ami-0f5ee92e2d63afc18"
+  ami                    = "ami-0c02fb55956c7d316"
   instance_type          = "t2.micro"
   subnet_id              = data.aws_subnet.public.id
-
   vpc_security_group_ids = ["sg-0ee7171f9f319c0e9"]
-
   associate_public_ip_address = true
 
   user_data = <<-EOF
@@ -14,7 +12,7 @@ resource "aws_instance" "web" {
               systemctl start httpd
               systemctl enable httpd
 
-              echo "<h1>🚀 SkinCare Terraform LIVE</h1>" > /var/www/html/index.html
+              echo "<h1>🚀 SkinCare Terraform Web Server LIVE</h1>" > /var/www/html/index.html
               EOF
 
   tags = {
